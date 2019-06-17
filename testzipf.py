49e7eb8f791f3a814cf43d6a8237631dde02470c
Andy Fraser <andy@fraserphysics.com>
2019-06-17 10:45:27 -0600

    Copied *py files from 09-latex

--- testzipf.py
+++ testzipf.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
-from countwords import load_word_counts
+""" Print a table of the 2 most frequent words in each book
+"""
+
 import sys
+import argparse
+
+from countwords import load_word_counts
 
 def top_two_word(counts):
     """
-    Given a list of (word, count, percentage) tuples, 
+    Given a list of (word, count, percentage) tuples,
     return the top two word counts.
     """
     limited_counts = counts[0:2]
@@ -12,16 +17,16 @@ def top_two_word(counts):
     return count_data
 
 def main(argv=None):
-    import argparse
-    
+
     if argv is None:                    # Usual case
         argv = sys.argv[1:]
-        
+
+    # Set up a parser and parse the command line arguements
     parser = argparse.ArgumentParser(
         description='Print a table of the 2 most frequent words in each book')
     parser.add_argument('data_files', nargs='*', help='list of data files')
     parser.add_argument('--latex', action='store_true',
-                        help='Use LaTeX formt for table')
+                        help='Use LaTeX format for table')
     args = parser.parse_args(argv)
 
     if args.latex:
@@ -30,13 +35,15 @@ def main(argv=None):
     else:
         header = "Book\tFirst\tSecond\tRatio"
         line_format = "%s\t%i\t%i\t%.2f"
-    
+
     print(header)
+
+    # Print the analysis for each book
     for input_file in args.data_files:
         counts = load_word_counts(input_file)
         [first, second] = top_two_word(counts)
         bookname = input_file[:-4]
         print(line_format%(bookname, first, second, float(first)/second))
-    
+
 if __name__ == '__main__':
     sys.exit(main())
